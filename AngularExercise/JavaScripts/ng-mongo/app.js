0c40815bc3f01aa12a20e21aa54c062877057d20
Mike <mike@marketyze.com>
2015-04-16 23:33:27 +0300

    First Commit

---
+++ AngularExercise/JavaScripts/ng-mongo/app.js
@@ -0,0 +1,8 @@
+﻿//var myCtrl = function ($scope) {
+//    $scope.items= [{ name: "Robe" }, { name: "Sally" }]
+//}
+
+//Tooken from http://stackoverflow.com/questions/17289236/error-argument-is-not-a-function-got-undefined
+angular.module('myApp', []).controller('myCtrl', function ($scope) {
+    $scope.items = [{ name: "Robe" }, { name: "Sally" }]
+});
